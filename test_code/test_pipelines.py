59a3f2cd803b6495ba0065f721aa47af8f9514fc
Vijay Dipak Takbhate <vijaytakbhate20@gmail.com>
2025-11-09 13:41:10 +0530

    Local Build Testing Done

--- test_code/test_pipelines.py
+++ test_code/test_pipelines.py
@@ -9,13 +9,26 @@ PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 SRC_PATH = os.path.join(PROJECT_ROOT, "src")
 sys.path.extend([SRC_PATH, PROJECT_ROOT])
 
+print(PROJECT_ROOT)
+
 from data_processing_pipeline import processData
 from inverse_data_processing_pipeline import inverseProcessData
 configs = json.load(open(os.path.join(PROJECT_ROOT, "config.json")))
 
-@pytest.fixture(scope="session")
+
 def test_process_pipelines():
-    df = pd.read_csv(os.path.join(PROJECT_ROOT, "test_row_data.xlsx"))
+    df = pd.read_csv(os.path.join(PROJECT_ROOT, "test_code/test_row_data.csv"))
     processed_data = processData(df)
+    processed_data.to_csv(os.path.join(PROJECT_ROOT, "test_code/test_processed_data.csv"), index=False)
     assert processed_data.shape[0] == df.shape[0]
-    assert processed_data.columns[1]  == df.shape[1]
+    assert processed_data.shape[1]  == df.shape[1]
+
+
+def test_inverse_process_pipelines():
+    df = pd.read_csv(os.path.join(PROJECT_ROOT, "test_code/test_processed_data.csv"))
+    original_data = inverseProcessData(df)
+    assert original_data.shape[0] == df.shape[0]
+    assert original_data.shape[1]  == df.shape[1]
+
+test_process_pipelines()
+test_inverse_process_pipelines()
